da6c3ee50f7d61e91f1f715281495ac6674fd511
vcooper <vanessa.cooper1@csuglobal.edu>
2025-04-21 14:19:51 -0500

    Added call to isPrime. Function still needs to be created.

--- src/PrimeNumberChecker.java
+++ src/PrimeNumberChecker.java
@@ -6,5 +6,18 @@ public class PrimeNumberChecker {
 
         System.out.print("Enter a positive integer: ");
         int number = scanner.nextInt();
+
+        if (number <= 1) {
+            System.out.println(number + " is not a prime number.");
+        } else {
+            boolean isPrime = isPrime(number);
+            if (isPrime) {
+                System.out.println(number + " is a prime number.");
+            } else {
+                System.out.println(number + " is not a prime number.");
+            }
+        }
+
+        scanner.close();
     }
 }

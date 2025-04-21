a96c7a5e697fd330af0a2496ca16c4efd62e3e01
vcooper <vanessa.cooper1@csuglobal.edu>
2025-04-21 14:21:10 -0500

    Added the isPrime function. Code is now complete.

--- src/PrimeNumberChecker.java
+++ src/PrimeNumberChecker.java
@@ -20,4 +20,15 @@ public class PrimeNumberChecker {
 
         scanner.close();
     }
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }

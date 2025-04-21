ae8b393799d221c1fee7b069c74abfc145549ce1
vcooper <vanessa.cooper1@csuglobal.edu>
2025-04-21 14:17:02 -0500

    Added in Scanner to prompt for number.

--- src/PrimeNumberChecker.java
+++ src/PrimeNumberChecker.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class PrimeNumberChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+    }
 }
